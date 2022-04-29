175ea24d73d5f793231ef84bae7b4a7c6fdd5125
caochen <1124904301@qq.com>
2022-04-29 15:45:09 +0800

    chore:给useDebounce增加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,7 +22,7 @@ export const useMount = (callback: () => void) => {
 }
 
 // 用泛型来规范类型
-export const useDebounce = (value: unknown, delay?: number): any => {
+export const useDebounce = <V>(value: V, delay?: number) => {
     const [debouncedValue, setDebouncedValue] = useState(value);
     // console.log(value.mayExist);
 
