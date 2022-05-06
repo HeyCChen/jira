ba6b63bcf2836dc3373b7f8334f341453d899df7
caochen <1124904301@qq.com>
2022-05-06 17:13:10 +0800

    feat:用row布局页面

--- src/authenticatedApp.tsx
+++ src/authenticatedApp.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useAuth } from './context/authContext';
 import { ProjectListScreen } from './screens/projectList';
+import styled from '@emotion/styled';
+import { Row } from './components/lib';
 
 export const AuthenticatedApp = () => {
 
@@ -8,8 +10,39 @@ export const AuthenticatedApp = () => {
 
     return (
         <>
-            <button onClick={logout}>登出用户</button>
-            <ProjectListScreen />
+            <Container>
+                <Header between={true}>
+                    <Headerleft gap={true}>
+                        <h3>logo</h3>
+                        <h3>项目</h3>
+                        <h3>用户</h3>
+                    </Headerleft>
+                    <HeaderRight>
+                        <button onClick={logout}>登出</button>
+                    </HeaderRight>
+                </Header>
+                <Main>
+                    <ProjectListScreen />
+                </Main>
+            </Container>
         </>
     )
 }
+
+const Container = styled.div`
+    display: grid;
+    grid-template-rows: 6rem 1fr;
+    height: 100vh;
+`
+
+const Header = styled(Row)`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
+const Headerleft = styled(Row)``
+
+const HeaderRight = styled.div``
+
+const Main = styled.main``
