3841083f7a27da832b0cd073b1f1574425be6c8e
caochen <1124904301@qq.com>
2022-05-06 16:21:28 +0800

    style:添加emotion完成登录注册页面样式

--- src/unauthenticatedApp/resgister.tsx
+++ src/unauthenticatedApp/resgister.tsx
@@ -1,5 +1,6 @@
-import { Button, Form, Input } from 'antd';
+import { Form, Input } from 'antd';
 import React from 'react';
+import { LongButton } from '.';
 import { useAuth } from '../context/authContext';
 
 
@@ -24,7 +25,7 @@ export const RegisterScreen = () => {
                     <Input placeholder='密码' type='password' id={'password'} />
                 </Form.Item>
                 <Form.Item>
-                    <Button htmlType='submit' type='primary'>注册</Button>
+                    <LongButton htmlType='submit' type='primary'>注册</LongButton>
                 </Form.Item>
             </Form>
         </>
